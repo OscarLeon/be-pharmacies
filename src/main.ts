dd6fac30ad0a3063d29271d81dbab4e31bdad3dd
Oscar <oscar.leon@4agile.cl>
2020-08-28 18:49:26 -0400

    ADD: helmet security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Security
+  app.use(helmet());
+
   await app.listen(3000);
+  console.log('Runing on port ==> ', 3000);
 }
 bootstrap();
