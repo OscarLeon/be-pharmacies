dc0b6eaa5d374489f9e2f484807bf2f4ab3f0aa4
Oscar <oscar.leon@4agile.cl>
2020-08-28 15:42:56 -0400

    UPDATE: comment import pharmacy(not implemeted)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { CommuneModule } from './modules/commune/commune.module';
-import { PharmacyModule } from './modules/pharmacy/pharmacy.module';
+// import { PharmacyModule } from './modules/pharmacy/pharmacy.module';
 
 @Module({
-  imports: [CommuneModule, PharmacyModule],
+  imports: [CommuneModule],
 })
 export class AppModule {}
